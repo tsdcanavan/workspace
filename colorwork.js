c3f3827ddfb761e3dd23eb3a6789ec680a91e29a
unknown <tsdcanavan@yahoo.com>
2017-10-03 13:39:33 -0400

    clean up colorwork.js

--- colorwork.js
+++ colorwork.js
@@ -2,46 +2,19 @@ var numbers = [];
 
 // load hex number list 0 - 15
 for (var i = 0; i < 16; i++) {
-    if (i < 10) {
-        numbers[i] = i.toString();
-    } else {
-        switch (i) {
-            case 10:
-                numbers[i] = "a";
-                break;
-            case 11:
-                numbers[i] = 'b';
-                break;
-            case 12:
-                numbers[i] = 'c';
-                break;
-            case 13:
-                numbers[i] = 'd';
-                break;
-            case 14:
-                numbers[i] = 'e';
-                break;
-            case 15:
-                numbers[i] = 'f';
-                break;
-            default:
-                break;
-        }
-    }
+    numbers[i] = i.toString(16);
 }
-
 
 // build hex color code
 var hexCode = '';
 for (i = 0; i < 6; i++) {
     var rndmNumber = Math.floor(Math.random() * 16);
     hexCode = hexCode + numbers[rndmNumber];
-//    console.log(rndmNumber, numbers[rndmNumber], hexCode);
 }
 
 var addHtml = $('<div>');
-addHtml.attr('class','jumbotron');
-addHtml.attr('style','background-color: #' + hexCode);
+addHtml.attr('class', 'jumbotron');
+addHtml.attr('style', 'background-color: #' + hexCode);
 addHtml.html('<h1>The color is ' + hexCode + '</h1>');
 $('.container').html(addHtml);
 // display code
