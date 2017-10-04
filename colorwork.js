4d0e174b2965203615030a7da415972951f8f437
unknown <tsdcanavan@yahoo.com>
2017-10-03 20:26:24 -0400

    commit work stuff

--- colorwork.js
+++ colorwork.js
@@ -11,17 +11,17 @@ for (i = 0; i < 6; i++) {
     var rndmNumber = Math.floor(Math.random() * 16);
     hexCode = hexCode + numbers[rndmNumber];
 }
-console.log(numbers);
-
+console.log(hexCode.charAt(0));
+var hexCodeRem = parseInt(hexCode.charAt(0), 16) % 2
+console.log(hexCodeRem);
 if ( typeof jQuery !== 'undefined') {
     var addHtml = $('<div>');
     addHtml.attr('class', 'jumbotron');
     addHtml.html('<h1>The color is ' + hexCode + '</h1>');
-    console.log(hexCode.charAt(0));
-    if (hexCode.charAt(0) === '0') {
-        addHtml.attr('style', 'background-color: #' + hexCode + '; color: #fff;');
+    if (hexCodeRem) {
+        addHtml.attr('style', 'background-color: #' + hexCode + '; color: #fff; text-align: center;');
     } else {
-        addHtml.attr('style', 'background-color: #' + hexCode + '; color: #000;');
+        addHtml.attr('style', 'background-color: #' + hexCode + '; color: #000; text-align: center;');
     }
     $('.container').html(addHtml);
 } 
